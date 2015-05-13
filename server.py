8bc2fded6ccb101b31e2170e5625ad0f10844c2e
sl591 <stefan.luedtke2@uni-rostock.de>
2015-05-13 20:00:50 +0200

    Zaehlt jetzt aktive User und zeigt an, dass Werbung vorbei, wenn die Mehrheit werbung vorbei gedrueckt hat

--- server.py
+++ server.py
@@ -1,45 +1,90 @@
 from wsgiref.util import setup_testing_defaults
 from wsgiref.simple_server import make_server
+from _thread import start_new_thread
+import time
 
-# Dient erst mal nur dazu, zu zeigen, dass Kommunikation zwischen Client und
-# Server möglich ist und Werte gespeichert werden können. Das Speichern in einer
-# Datei ist sicherlich nicht ideal
+#dictionary der aktiven user
+users={}
+#dictionary der user, die den button "werbung vorbei" gedrueckt haben
+adoverusers={}
 
 
 #Mit dieser Methode beantwortet der Server Anfragen.
 #Für eine Übersicht über die Daten, die in environ stehen, siehe:
 #https://www.python.org/dev/peps/pep-0333/#environ-variables
 def simple_app(environ, start_response):
-
-    if environ["PATH_INFO"] == "/anzahl.txt":
-        #wenn anzahl.txt angefragt wird:
-        #anzahl aus datei einlesen, um 1 erhoehen, und zurueckliefern
-        f = open('anzahl.txt', 'r')
-        anzahl = int(f.read())
-        f.close()
-        anzahl += 1
-        f = open('anzahl.txt','w')
-        f.write(str(anzahl))
-        f.close()
-        start_response('200 OK', [])
-        return [str(anzahl).encode('utf-8')]
-    elif environ["PATH_INFO"] == "/":
-        #wenn / angefragt: index.html liefern
+    #heartbeat-signal
+    if environ["QUERY_STRING"].startswith("heartbeat"):
+        return handle_heartbeat(environ,start_response)
+    #user hat button "werbung vorbei" gedrueckt
+    elif environ["QUERY_STRING"].startswith("button_adover"):
+        return handle_adover(environ,start_response);
+    #wenn / angefragt: index.html liefern
+    elif environ["PATH_INFO"] == "/" and environ["QUERY_STRING"] == "":
         f=open('index.html','r')
         file=f.read()
         start_response('200 OK', [])
-        return [file.encode('utf-8')]        
-    else:
-        #sonst einfach angefragte datei liefern (unsicher?)
+        return [file.encode('utf-8')]
+    #sonst einfach angefragte datei liefern (unsicher?)
+    elif environ["QUERY_STRING"] == "":
         f=open(environ["PATH_INFO"][1:],'r')
         file=f.read()
         start_response('200 OK', [])
         return [file.encode('utf-8')]
-        
 
+        
+def handle_heartbeat(environ,start_response):
+    global users
+    query=environ["QUERY_STRING"].split(":")
+    uid=query[1]
+    # wenn uid noch nicht in users: neu hinzu, sonst wird automatisch
+    # vorhandener wert an stelle uid mit True ueberschrieben
+    users.update({uid:True})
+    start_response('200 OK', [])
+    print("Heartbeat von "+uid)
+    return [str(len(users)).encode('utf-8')]
+
+def handle_adover(environ,start_response):
+    global adoverusers
+    query=environ["QUERY_STRING"].split(":")
+    uid=query[1]
+    # wenn uid noch nicht in adoverusers: neu hinzu, sonst wird einfach
+    # vorhandener wert ueberschrieben
+    adoverusers.update({uid:True})
+    #testen, ob Mehrheit Button Werbung vorbei gedrueckt hat
+    if len(adoverusers)>1/2*len(users):
+        response="adover"
+        #Werbung vorbei, adoverusers loeschen
+        adoverusers={}
+    else:
+        response="adnotover"
+    start_response('200 OK', [])
+    print("Werbung vorbei bei "+uid)
+    return [response.encode('utf-8')]
+
+    
 #Server erzeugen, der die Methode simple_app zur Beantwortung von Anfragen verwendet
 httpd = make_server('', 8000, simple_app)
 print("Serving HTTP on port 8000...")
+
+#Thread starten, der alle 2 min im users-dictionary aufraemt (alle user entfernen,
+#von denen dann noch kein heartbeat erhalten wurde
+def cleanupusers():
+    global users
+    while True:
+        time.sleep(120)
+        print("Aufraeumen")
+        newusers=dict(users)
+        for uid in users:
+            if users[uid]==True:
+                newusers[uid]=False
+            else:
+                del newusers[uid]
+        users=newusers
+            
+start_new_thread(cleanupusers,())
+
+
 
 # Beantworte dauerhaft Anfragen
 httpd.serve_forever()
